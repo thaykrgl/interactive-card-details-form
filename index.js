de9c0d526245396386f9bc3d6b1f032d7277a178
Taha Aykıroğlu <tahaaykiroglu99@gmail.com>
2022-08-10 18:59:01 +0300

    Added input validations

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ $(document).ready(function () {
   let holderNumberDOM = document.getElementById("holder-number");
   let expDateMonthDOM = document.getElementById("exp-date-mm");
   let expDateYearDOM = document.getElementById("exp-date-yy");
+  let cvcDOM = document.getElementById("cvc");
+  let expDateMonthWarningDOM = document.getElementById("mm-warning");
+  let expDateYearWarningDOM = document.getElementById("yy-warning");
+  let cvcWarningDOM = document.getElementById("cvc-warning");
 
 
   // CardHolder Name
@@ -64,28 +68,62 @@ $(document).ready(function () {
     this.value = this.value.replace(/[^\d]/, '')
     document.getElementById("card-cvc").innerHTML = e.target.value
   });
+
+  // Credit Card Number Format Function
+  function cc_format(value) {
+    var v = value.replace(/\s+/g, '').replace(/[^0-9]/gi, '')
+    var matches = v.match(/\d{4,16}/g);
+    var match = matches && matches[0] || ''
+    var parts = []
+    for (i = 0, len = match.length; i < len; i += 4) {
+      parts.push(match.substring(i, i + 4))
+    }
+    if (parts.length) {
+      return parts.join(' ')
+    } else {
+      return value
+    }
+  }
+
+  // Credit Card Expiry Dates Format Function
+  function cc_expires_format(value) {
+    return value.replace(
+      /[^0-9]/g, '' // To allow only numbers
+    );
+  }
+
+  // Confirm Button Click Event
+  document.getElementById("confirm").addEventListener("click", function () {
+    if (!expDateMonthDOM.value) {
+      expDateMonthWarningDOM.classList.remove("d-none");
+      expDateMonthDOM.classList.add("btn-warning");
+
+    }
+    else {
+      expDateMonthWarningDOM.classList.add("d-none");
+      expDateMonthDOM.classList.remove("btn-warning");
+    }
+
+    if (!expDateYearDOM.value) {
+      expDateYearWarningDOM.classList.remove("d-none");
+      expDateYearDOM.classList.add("btn-warning");
+    }
+    else {
+      expDateYearWarningDOM.classList.add("d-none");
+      expDateYearDOM.classList.remove("btn-warning");
+    }
+
+    if (!cvcDOM.value) {
+      cvcWarningDOM.classList.remove("d-none");
+      cvcDOM.classList.add("btn-warning");
+    }
+    else {
+      cvcWarningDOM.classList.add("d-none");
+      cvcDOM.classList.remove("btn-warning");
+
+    }
+  })
+
 })
 
 
-// Credit Card Number Format Function
-function cc_format(value) {
-  var v = value.replace(/\s+/g, '').replace(/[^0-9]/gi, '')
-  var matches = v.match(/\d{4,16}/g);
-  var match = matches && matches[0] || ''
-  var parts = []
-  for (i = 0, len = match.length; i < len; i += 4) {
-    parts.push(match.substring(i, i + 4))
-  }
-  if (parts.length) {
-    return parts.join(' ')
-  } else {
-    return value
-  }
-}
-
-// Credit Card Expiry Dates Format Function
-function cc_expires_format(value) {
-  return value.replace(
-    /[^0-9]/g, '' // To allow only numbers
-  );
-}
